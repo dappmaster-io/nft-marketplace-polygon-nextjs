33c7c572c9315b783336adcaeb5f4c856862b33f
brandon9598 <brandon9598@gmail.com>
2022-03-24 22:49:32 -0400

    Update home page to include a header and show NFTs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
 import '../styles/globals.css'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">Metaverse Marketplace</p>
+        <div className="mt-4 flex">
+          <Link href="/">
+            <a className="mr-4 text-pink-500">Home</a>
+          </Link>
+          <Link href="/create-item">
+            <a className="mr-6 text-pink-500">Sell Digital Assets</a>
+          </Link>
+          <Link href="/my-assets">
+            <a className="mr-6 text-pink-500">My Digital Assets</a>
+          </Link>
+          <Link href="/creator-dashboard">
+            <a className="mr-6 text-pink-500">Creator Dashboard</a>
+          </Link>
+        </div>
+      </nav>
+    </div>
+  )
 }
 
 export default MyApp
