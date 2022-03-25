5345dd11e76a4e6dd13fa0ed0d4a2eb173ad42e1
brandon9598 <brandon9598@gmail.com>
2022-03-24 23:32:19 -0400

    worked on asset page and create item page

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ const Home = () => {
 
     const items = await Promise.all(
       data.map(async (i) => {
-        const tokenUri = await tokenContract.tokenUri(i.tokenId)
+        const tokenUri = await tokenContract.tokenURI(i.tokenId)
         const meta = await axios.get(tokenUri)
         let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
         let item = {
